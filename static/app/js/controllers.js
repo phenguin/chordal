76ccc4b43c9eb404846ac1be6a1a8d015186bd7b
Phenguin <jcullen@mit.edu>
2013-10-23 05:39:19 -0400

    Factor out api calls into service

--- static/app/js/controllers.js
+++ static/app/js/controllers.js
@@ -2,8 +2,7 @@
 
 /* Controllers */
 angular.module('myApp.controllers', []).
-    controller('FretboardCtrl', ['$scope', '$http', 'Music','$routeParams', function($scope, $http, Music, $routeParams) {
-    var domain = "http://localhost:8080";
+    controller('FretboardCtrl', ['$scope', '$http', 'Music', 'Api','$routeParams', function($scope, $http, Music, Api, $routeParams) {
     var encodedurl=$routeParams.encodedurl;
     var url;
     console.log(encodedurl);
@@ -24,60 +23,14 @@ angular.module('myApp.controllers', []).
     $scope.current_chord = {};
     $scope.response = {};
 
-    $scope.getVoicing = function () {
-        var params = {
-            note : $scope.note,
-            chordType : $scope.chordType,
-            tuning : $scope.tuning,
-            lowRange : $scope.lowRange,
-            highRange : $scope.highRange
-        };
+    if (encodedurl) {
+        url = atob(encodedurl);
 
-        $http({ url : domain + '/api/voicing_in_range',
-              method : 'GET',
-              params : params
-        }).success(function (data) {
-            $scope.response = data;
-            $scope.current_chord = data;
-        });
-
-    };
-
-    $scope.updateRangeAndQuery = function (startFret) {
-        $scope.lowRange = startFret;
-        $scope.highRange = startFret + 4;
-        $scope.parseChords();
-     
-    };
-
-    $scope.parseChords = function () {
-        var params = {
-            queryString : $scope.chordString,
-            tuning : $scope.tuning,
-            lowRange : $scope.lowRange,
-            highRange : $scope.highRange
-        };
-
-        $http({ url : domain + '/api/parse_chords_with_voice_leading',
-              method : 'GET',
-              params : params
-        }).success(function (data) {
-            var chordNames;
-
-            chordNames = $scope.chordString.replace(/\s+/, ' ').split(' ');
-
-            _.each(_.range($scope.active_chords.length), function (i) {
-                data[i].chordName = chordNames[i];
-            });
-
-            $scope.active_chords = data;
-
-            console.log($scope.active_chords);
-
-            $scope.current_chord = $scope.active_chords[0];
-        });
-
-    };
+        $scope.active_chords = Api.chordsFromUrlPromise(url, $scope.tuning, $scope.lowRange, $scope.highRange);
+        
+        console.log("Active chords:");
+        console.log($scope.active_chords);
+    }
 
     $scope.setCurrentChord = function (chord) {
         $scope.current_chord = chord;
@@ -88,101 +41,5 @@ angular.module('myApp.controllers', []).
         $scope.current_chord = {};
     }
 
-    $scope.parseChordsAndAdd = function () {
-        var params = {
-            queryString : $scope.chordString,
-            tuning : $scope.tuning,
-            lowRange : $scope.lowRange,
-            highRange : $scope.highRange
-        };
 
-        $http({ url : domain + '/api/parse_chord',
-              method : 'GET',
-              params : params
-        }).success(function (data) {
-            var chordNames;
-
-            chordNames = $scope.chordString.replace(/\s+/, ' ').split(' ');
-
-            _.each(_.range(data.length), function (i) {
-                data[i].chordName = chordNames[i];
-            });
-
-            console.log(data);
-
-            $scope.active_chords = $scope.active_chords.concat(data);
-            $scope.current_chord = data[data.length - 1];
-        });
-
-    };
-
-    $scope.parseChordsFromUrl = function () {
-        var params = {
-            url : $scope.urlString,
-            tuning : $scope.tuning,
-            lowRange : $scope.lowRange,
-            highRange : $scope.highRange
-        };
-
-        $http({ url : domain + '/api/chords_from_url',
-              method : 'GET',
-              params : params
-        }).success(function (data) {
-
-            _.each(_.range(data.length), function (i) {
-                data[i].chordName = "<chordname>";
-            });
-
-            console.log(data);
-
-            $scope.active_chords = data;
-            $scope.current_chord = data[0];
-        });
-
-    };
-
-    if (encodedurl) {
-        url = atob(encodedurl);
-        $scope.urlString = url;
-        $scope.parseChordsFromUrl();
-        console.log("Active chords:");
-        console.log($scope.active_chords);
-    }
-
-
-    $scope.updateFretboard = function  () {
-        var params = { note : $scope.note,
-            chordType : $scope.chordType,
-            tuning : $scope.tuning };
-
-            $http({ url : domain + '/api/chord_notes',
-                  method : 'GET',
-                  params : params
-            }).success(function (data) {
-                $scope.current_chord = data;
-            });
-
-    };
-
-
-
-    $scope.getInterval = function (i,j) {
-        var val = $scope.current_chord["fret_" + i + "_" + j];
-        if (val) {
-            var ret_val;
-            if (val.interval.length === 2) {
-                var res = val.interval[0] === 'b' ? '\u266d' : val.interval[0];
-                var res = val.interval[0] === '#' ? '\u266f' : val.interval[0];
-                ret_val = res + val.interval[val.interval.length - 1];
-            } else if (val.interval === '1') {
-                ret_val = 'R';
-            } else {
-                ret_val = val.interval;
-            }
-            return ret_val;
-        }
-        else {
-            return false;
-        }
-    };
 }]);
--- static/app/js/services.js
+++ static/app/js/services.js
@@ -11,32 +11,154 @@ myAppServices.value('version', '0.1');
 
 myAppServices.factory('Music', function () {
     var notes = [
-            { value : 'A', text : 'A' },
-            { value : 'Bb', text : 'B\u266d' },
-            { value : 'C', text : 'C' },
-            { value : 'Db', text : 'D\u266d' },
-            { value : 'D', text : 'D' },
-            { value : 'Eb', text : 'E\u266d' },
-            { value : 'E', text : 'E' },
-            { value : 'F', text : 'F' },
-            { value : 'Gb', text : 'G\u266d' },
-            { value : 'G', text : 'G' },
-            { value : 'Ab', text : 'A\u266d' }
-            ];
+        { value : 'A', text : 'A' },
+        { value : 'Bb', text : 'B\u266d' },
+        { value : 'C', text : 'C' },
+        { value : 'Db', text : 'D\u266d' },
+        { value : 'D', text : 'D' },
+        { value : 'Eb', text : 'E\u266d' },
+        { value : 'E', text : 'E' },
+        { value : 'F', text : 'F' },
+        { value : 'Gb', text : 'G\u266d' },
+        { value : 'G', text : 'G' },
+        { value : 'Ab', text : 'A\u266d' }
+    ];
 
     var chordTypes = [
-            { value : 'majorTriad', text : 'Major Triad' },
-            { value : 'minorTriad', text : 'Minor Triad' },
-            { value : 'augTriad', text : 'Augmented Triad' },
-            { value : 'dimTriad', text : 'Diminished Triad' },
-            { value : 'maj7chord', text : 'Major 7th Chord' },
-            { value : 'min7chord', text : 'Minor 7th Chord' },
-            { value : 'dom7chord', text : 'Dominant 7th Chord' },
-            { value : 'halfdim7chord', text : 'Half Diminished Chord' },
-            { value : 'fulldim7chord', text : 'Fully Diminished Chord' }
-            ];
+        { value : 'majorTriad', text : 'Major Triad' },
+        { value : 'minorTriad', text : 'Minor Triad' },
+        { value : 'augTriad', text : 'Augmented Triad' },
+        { value : 'dimTriad', text : 'Diminished Triad' },
+        { value : 'maj7chord', text : 'Major 7th Chord' },
+        { value : 'min7chord', text : 'Minor 7th Chord' },
+        { value : 'dom7chord', text : 'Dominant 7th Chord' },
+        { value : 'halfdim7chord', text : 'Half Diminished Chord' },
+        { value : 'fulldim7chord', text : 'Fully Diminished Chord' }
+    ];
 
     return { notes : notes, chordTypes : chordTypes };
+}).
+    factory('Api', function ($http) {
+
+    var res = {};
+    var domain = "http://localhost:8080";
+
+    res.chordsFromUrlPromise = function (url, tuning, low, high) {
+        var params = {
+            url : url,
+            tuning : tuning,
+            lowRange : low,
+            highRange : high
+        };
+
+        var promise = $http({ url : domain + '/api/chords_from_url',
+              method : 'GET',
+              params : params
+        });
+
+        promise._resolved = false;
+
+        return promise.then(function (response) {
+            var res = response.data;
+            res._resolved = true;
+            return res;
+        });
+
+    };
+    res.voicingPromise = function (note, chordType, tuning, lowRange, highRange) {
+        var params = {
+            note : note,
+            chordType : chordType,
+            tuning : tuning,
+            lowRange : lowRange,
+            highRange : highRange
+        };
+
+        var promise = $http({ url : domain + '/api/voicing_in_range',
+              method : 'GET',
+              params : params
+        });
+
+        promise._resolved = false;
+
+        return promise.then(function (response) {
+            var res = response.data;
+            res._resolved = true;
+            return res;
+        });
+
+    };
+
+    res.parsedChordsVLPromise = function (query, tuning, low, high) {
+        var params = {
+            queryString : query,
+            tuning : tuning,
+            lowRange : low,
+            highRange : high
+        };
+
+        var promise = $http({ url : domain + '/api/parse_chords_with_voice_leading',
+              method : 'GET',
+              params : params
+        });
+
+        promise._resolved = false;
+        return promise.then(function (response) {
+
+            var chordNames;
+            var data = response.data;
+
+            chordNames = query.replace(/\s+/, ' ').split(' ');
+
+            _.each(_.range(data.length), function (i) {
+                data[i].chordName = chordNames[i];
+            });
+
+            console.log("VL");
+            console.log(data);
+            data._resolved = true;
+            return data;
+
+        });
+
+
+    };
+
+    res.parsedChordsPromise = function (query, tuning, low, high) {
+        var params = {
+            queryString : query,
+            tuning : tuning,
+            lowRange : low,
+            highRange : high
+        };
+
+        var promise = $http({ url : domain + '/api/parse_chord',
+              method : 'GET',
+              params : params
+        })
+
+        promise._resolved = false;
+
+        return promise.then(function (response) {
+            var chordNames;
+            var data = response.data;
+
+            chordNames = query.replace(/\s+/, ' ').split(' ');
+
+            _.each(_.range(data.length), function (i) {
+                data[i].chordName = chordNames[i];
+            });
+
+            console.log("No VL");
+            console.log(data);
+            data._resolved = true;
+            return data;
+
+        });
+
+    };
+    return res;
+
 });
 
 
